42a847070316efc1e4cbf8e2e0711bd25edb4c2d
Johnny Richardson <johnny.c.rich@gmail.com>
2025-12-08 17:48:39 -0500

    feat(pages/api/cookie.ts): aPI route to check if user has stored cookie, decrypt it, return tokens

---
+++ pages/api/cookie.ts
@@ -0,0 +1,40 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import decryptCookie from "../../utils/Decrypt";
+import { JWTDecryptResult } from "jose";
+
+/**
+ * API Route to simply see if user has stored cookie and then decrypt it, return tokens
+ * @param req - NextApiRequest object
+ * @param res - NextApiResponse object
+ */
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  let apiResponse;
+  let responseCode = 200;
+  const token = req.cookies["nextspace-session"] || null;
+
+  if (!token) {
+    responseCode = 401;
+    apiResponse = { error: "No session cookie found" };
+  } else {
+    try {
+      const decrypted: JWTDecryptResult = await decryptCookie(token);
+      apiResponse = {
+        tokens: {
+          access: decrypted.payload.access,
+          refresh: decrypted.payload.refresh,
+        },
+        userId: decrypted.payload.userId,
+        username: decrypted.payload.sub,
+      };
+    } catch (error) {
+      console.error("Failed to decrypt cookie:", error);
+      responseCode = 500;
+      apiResponse = { error: "Failed to decrypt cookie" };
+    }
+  }
+
+  return res.status(responseCode).json(apiResponse);
+}
