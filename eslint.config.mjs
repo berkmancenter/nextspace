f8ac2e1a92f00c0a7fb0e8e24076a94fe16a338d
Jennifer Hickey <jhickey@cyber.harvard.edu>
2026-05-12 14:49:20 -0400

    style: add scripts and eslint config for prettier, add .prettierrc.json
    standardize prettier settings for line length, tab, single quote preference to prevent non-material
    changes b/w devs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,11 @@
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
 const compat = new FlatCompat({
   // import.meta.dirname is available after Node.js v20.11.0
   baseDirectory: import.meta.dirname,
 });
 const eslintConfig = [
   ...compat.config({
-    extends: ["next/core-web-vitals"],
+    extends: ['next/core-web-vitals', 'plugin:prettier/recommended'],
   }),
 ];
 export default eslintConfig;
