9f3f0a304f7613902a27f7e5946e16707314e24b
Johnny Richardson <johnny.c.rich@gmail.com>
2026-05-21 19:21:34 -0400

    refactor(utils/errorhandler.ts): update param handler to handle more cases

--- utils/ErrorHandler.ts
+++ utils/ErrorHandler.ts
@@ -1,16 +1,50 @@
 import { NextRouter } from 'next/router';
+import { GetChannelPasscode } from './Helpers';
+
+type Page = 'moderator' | 'assistant';
 
 /**
  * Checks if the router query parameters are valid for a conversation and returns an error message if they are not.
  * @param router - The Next.js router object.
+ * @param page - The current page type.
  */
-export const QueryParamsError = (router: NextRouter) => {
-  if (!router.query.conversationId || !router.query.passcode) {
-    let messageSuffix = 'a Conversation ID';
-    if (!router.query.conversationId && !router.query.passcode) messageSuffix = 'both a Conversation ID and Passcode';
-    else if (router.query.conversationId && !router.query.passcode) messageSuffix = 'a Passcode';
+export const QueryParamsError = (router: NextRouter, page: Page): { header: string; params: string[] } | null => {
+  const { conversationId, channel }: { conversationId?: string; channel?: string[] | string } = router.query;
+  const missing: string[] = [];
 
-    return `Please provide ${messageSuffix}.`;
+  const checkTranscriptPasscode = () => {
+    // Also check for transcript channel and passcode
+    if (typeof channel === 'string' && !channel.startsWith('transcript')) missing.push('transcript channel');
+    else if (Array.isArray(channel) && !channel.find((ch) => ch.startsWith('transcript')))
+      missing.push('transcript channel');
+    if (!GetChannelPasscode('transcript', router.query)) missing.push('transcript passcode');
+  };
+
+  if (!conversationId) missing.push('conversation ID');
+  if (!/^[0-9a-fA-F]{24}$/.test(conversationId || '')) missing.push('invalid conversation ID');
+  if (!channel)
+    // If channel missing entirely...
+    missing.push('channel');
+
+  if (page === 'moderator') {
+    // If only one channel specified, check if starts with 'moderator'
+    if (typeof channel === 'string' && !channel.startsWith('moderator')) missing.push('moderator channel');
+    else if (Array.isArray(channel) && !channel.find((ch) => ch.startsWith('moderator'))) missing.push('moderator channel');
+    if (!GetChannelPasscode('moderator', router.query)) missing.push('moderator passcode');
+
+    checkTranscriptPasscode();
   }
-  return 'Something went wrong';
+
+  if (page === 'assistant') {
+    // If only one channel specified, check if starts with 'chat'
+    if (typeof channel === 'string' && !channel.startsWith('chat')) missing.push('chat channel');
+    else if (Array.isArray(channel) && !channel.find((ch) => ch.startsWith('chat'))) missing.push('chat channel');
+    if (!GetChannelPasscode('chat', router.query)) missing.push('chat passcode');
+
+    checkTranscriptPasscode();
+  }
+
+  if (missing.length > 0) return { header: `Missing required parameter${missing.length > 1 ? 's' : ''}`, params: missing };
+
+  return null;
 };
