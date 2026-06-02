2caffcfd34a08655959e8fb8e950b9e9f5f5259d
Chelsea Johnson <72229300+cbj0hns0n@users.noreply.github.com>
2026-06-02 13:04:20 -0700

    feat: add edit button and edit event page

---
+++ pages/admin/[type]/edit/[id].tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { CircularProgress } from '@mui/material';
+import { CheckAuthHeader, getConversation } from '../../../../utils/Helpers';
+import { AuthType } from '../../../../types.internal';
+
+import { EventCreationForm } from '../../../../components';
+import { Conversation } from '../../../../types.internal';
+
+export const getServerSideProps = async (context: { req: any }) => {
+  return CheckAuthHeader(context.req.headers);
+};
+
+function EditEventScreen({ authType }: { authType: AuthType }) {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const [conversationData, setConversationData] = useState<Conversation | null>(null);
+
+  useEffect(() => {
+    if (!id) return;
+
+    const fetchConversationData = async () => {
+      try {
+        const data = await getConversation(id as string);
+        setConversationData(data);
+      } catch (error) {
+        console.error('Error fetching conversation data:', error);
+      }
+    };
+
+    fetchConversationData();
+  }, [id]);
+
+  return (
+    <div className="flex justify-center items-start">
+      <div className="mt-12 w-2/3">
+        {conversationData ? (
+          <EventCreationForm mode="edit" initialEvent={conversationData} />
+        ) : (
+          <div style={{ marginTop: 40 }}>
+            <CircularProgress />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default EditEventScreen;
